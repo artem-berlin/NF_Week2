5200c5c8b3a77ec1628bbf4fa1d5314403d7ae3f
artem-berlin <art.tester@gmail.com>
2022-02-09 09:25:39 +0100

    day2 test students

--- day2_Interface/src/main/java/com/nf/oop/Main.java
+++ day2_Interface/src/main/java/com/nf/oop/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-
+//ViolineStudent a = new ViolineStudent("artem","violine", 123);
+//CelloStudent   a1 = new CelloStudent("artem","cello",12345);
 
     }
 }
